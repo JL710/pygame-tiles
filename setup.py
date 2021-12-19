e46d840acba3735ec4c37b11cb1d470cec0fe2e7
JL710 <jan.lyncker@freenet.de>
2021-12-19 15:22:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
 	version='0.0.1',
 	description='Simple Tools for Python',
 	py_modules=["SimpleTools"],
-	package_dir={'': 'src', 'a1': 'a1', 'a2': 'a2'},
+	package_dir={'': 'src'},
+	packages=setuptools.find_packages(where="src")
 )
