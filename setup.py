184e5190c4df911c31a33ef05a829232ede7c90f
JL710 <jan.lyncker@freenet.de>
2021-12-19 15:17:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
+import setuptools
 
 setup(
-	name='pygame-tiles',
+	name='pygametiles',
 	version='0.0.1',
 	description='Simple Tools for Python',
 	py_modules=["SimpleTools"],
-	package_dir={'': 'src'}
+	package_dir={'': 'src'},
+	packages=setuptools.find_packages(where="src")
 )
